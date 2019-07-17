533f7ed95e616b1efae42015423f1132cf18ed28
John Codeinaire <johncodeinaire@gmail.com>
2019-07-17 17:06:18 +1000

    L47 - Cleaning up render prop mess

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -24,7 +24,7 @@ const TOGGLE_CART_MUTATION = gql`
 `;
 /* eslint-disable */
 const Composed = adopt({
-  user: <User />,
+  user: ({ render }) => <User>{render}</User>,
   toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
   localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
 });
@@ -33,7 +33,7 @@ const Composed = adopt({
 const Cart = () => (
   <Composed>
     {({ user, toggleCart, localState }) => {
-      const me = user.data.me;
+      const { me } = user.data;
       if (!me) return null;
       return (
         <CartStyles open={localState.data.cartOpen}>
@@ -43,10 +43,15 @@ const Cart = () => (
             </CloseButton>
             <Supreme>{me.name}'s Cart</Supreme>
             <p>
-              You Have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in your cart.
+              You Have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in
+              your cart.
             </p>
           </header>
-          <ul>{me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}</ul>
+          <ul>
+            {me.cart.map(cartItem => (
+              <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+          </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
             <SickButton>Checkout</SickButton>
