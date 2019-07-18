1e845d86a2ed98cad8ca587141cf4be371bd3475
John Codeinaire <johncodeinaire@gmail.com>
2019-07-18 12:26:36 +1000

    L49 - CC processing with stripe checkout

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -10,6 +10,7 @@ import SickButton from './styles/SickButton';
 import CartItem from './CartItem';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import formatMoney from '../lib/formatMoney';
+import TakeMyMoney from './TakeMyMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -33,7 +34,7 @@ const Composed = adopt({
 const Cart = () => (
   <Composed>
     {({ user, toggleCart, localState }) => {
-      const { me } = user.data;
+      const {me} = user.data;
       if (!me) return null;
       return (
         <CartStyles open={localState.data.cartOpen}>
@@ -54,7 +55,9 @@ const Cart = () => (
           </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <SickButton>Checkout</SickButton>
+            <TakeMyMoney>
+              <SickButton>Checkout</SickButton>
+            </TakeMyMoney>
           </footer>
         </CartStyles>
       );
